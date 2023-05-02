cc321c77a9650314128ac1a17320556c2aad7fc8
Jody VanHoose <jodyvanhoose80@gmail.com>
2023-05-02 18:46:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const countdownEl = document.getElementById('countdown')
+const displayBlockedEl = document.querySelector('.wait-til-8')
+const displayShowEl = document.querySelector('.open-on-8')
+
+const countdown = () =>{
+  const countDate = new Date('May 5, 2023 00:00:00').getTime()
+  const now = new Date().getTime()
+  const gap = countDate - now
+
+  const second = 1000
+  const minute = second * 60
+  const hour = minute * 60
+  const day = hour * 24
+  
+  const textDay = Math.floor(gap / day)
+  const textHour = Math.floor((gap % day) / hour)
+  const textMinute = Math.floor((gap % hour) / minute)
+  const textSecond = Math.floor((gap % minute) / second)
+  
+
+  
+
+  if(gap <= 0 ){
+    countdownEl.innerHTML = `<h1 class="big-text">Happy Aniversary</h1>`
+    displayBlockedEl.style.display = 'none'
+    displayShowEl.style.display = 'block'
+  }
+  else{
+    document.getElementById('days').innerText = textDay
+    document.getElementById('hours').innerText = textHour
+    document.getElementById('minutes').innerText = textMinute
+    document.getElementById('seconds').innerText = textSecond
+  }
+}
+
+setInterval(countdown, 1000)
+
